c611746d88f33ab3d24fa94fb3016c20df0476da
Loic Leprieur <loic@lenovo-flex2>
2015-12-09 18:20:20 +0100

    Mis à jour des fichiers - Ajout des diagrammes - Ajout du freeplane (brainstorming des fonctionnalités) - Supression des doublons et des fichiers inutiles

---
+++ Freeplane/PROJET.mm
@@ -0,0 +1,97 @@
+<map version="freeplane 1.3.0">
+<!--To view this file, download free mind mapping software Freeplane from http://freeplane.sourceforge.net -->
+<node TEXT="PROJET" ID="ID_1723255651" CREATED="1283093380553" MODIFIED="1448985710941"><hook NAME="MapStyle">
+
+<map_styles>
+<stylenode LOCALIZED_TEXT="styles.root_node">
+<stylenode LOCALIZED_TEXT="styles.predefined" POSITION="right">
+<stylenode LOCALIZED_TEXT="default" MAX_WIDTH="600" COLOR="#000000" STYLE="as_parent">
+<font NAME="SansSerif" SIZE="10" BOLD="false" ITALIC="false"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.details"/>
+<stylenode LOCALIZED_TEXT="defaultstyle.note"/>
+<stylenode LOCALIZED_TEXT="defaultstyle.floating">
+<edge STYLE="hide_edge"/>
+<cloud COLOR="#f0f0f0" SHAPE="ROUND_RECT"/>
+</stylenode>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.user-defined" POSITION="right">
+<stylenode LOCALIZED_TEXT="styles.topic" COLOR="#18898b" STYLE="fork">
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.subtopic" COLOR="#cc3300" STYLE="fork">
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.subsubtopic" COLOR="#669900">
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.important">
+<icon BUILTIN="yes"/>
+</stylenode>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.AutomaticLayout" POSITION="right">
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level.root" COLOR="#000000">
+<font SIZE="18"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,1" COLOR="#0033ff">
+<font SIZE="16"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,2" COLOR="#00b439">
+<font SIZE="14"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,3" COLOR="#990000">
+<font SIZE="12"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,4" COLOR="#111111">
+<font SIZE="10"/>
+</stylenode>
+</stylenode>
+</stylenode>
+</map_styles>
+</hook>
+<hook NAME="AutomaticEdgeColor" COUNTER="6"/>
+<node TEXT="Jeu" POSITION="right" ID="ID_1649029255" CREATED="1448984421778" MODIFIED="1448984454931">
+<edge COLOR="#ff00ff"/>
+<node TEXT="Trouver expression" ID="ID_797645067" CREATED="1448984588773" MODIFIED="1448984602707"/>
+<node TEXT="D&#xe9;placer joueur" ID="ID_1254151075" CREATED="1448984604809" MODIFIED="1448986201553">
+<node TEXT="D&#xe9;placer au nord" ID="ID_1211717404" CREATED="1448984614270" MODIFIED="1448986229405"/>
+<node TEXT="D&#xe9;placer &#xe0; l&apos;est" ID="ID_1093564150" CREATED="1448984624086" MODIFIED="1448986230909"/>
+<node TEXT="D&#xe9;placer au sud" ID="ID_1220058222" CREATED="1448984629654" MODIFIED="1448986235591"/>
+<node TEXT="D&#xe9;placer &#xe0; l&apos;ouest" ID="ID_933115871" CREATED="1448984645841" MODIFIED="1448986237401"/>
+</node>
+<node TEXT="G&#xe9;n&#xe9;rer carte" ID="ID_988978362" CREATED="1448984663069" MODIFIED="1448984670726">
+<node TEXT="Dessiner les salles" ID="ID_709247540" CREATED="1448984676801" MODIFIED="1448984680175"/>
+<node TEXT="Dessiner les liens" ID="ID_678865447" CREATED="1448984681405" MODIFIED="1448984698870"/>
+</node>
+<node TEXT="Modifier environnement" ID="ID_1743471232" CREATED="1448984714261" MODIFIED="1448986249683">
+<node TEXT="Donner description objet/lieu" ID="ID_51119890" CREATED="1448984752787" MODIFIED="1448986289251"/>
+<node TEXT="G&#xe9;rer inventaire" ID="ID_1365643002" CREATED="1448984925135" MODIFIED="1448985080293">
+<node TEXT="Ins&#xe9;rer objet" ID="ID_289397706" CREATED="1448985096086" MODIFIED="1448985101695"/>
+<node TEXT="Retirer objet" ID="ID_626783936" CREATED="1448985102211" MODIFIED="1448985111942"/>
+</node>
+</node>
+<node TEXT="Afficher inventaire" ID="ID_1608693317" CREATED="1448984802092" MODIFIED="1448984810401">
+<node TEXT="Lister objet" ID="ID_849982976" CREATED="1448984813433" MODIFIED="1448984820310"/>
+<node TEXT="Chercher objet" ID="ID_473255402" CREATED="1448984820819" MODIFIED="1448984923225"/>
+</node>
+</node>
+<node TEXT="Editeur" POSITION="left" ID="ID_1014048636" CREATED="1448984443803" MODIFIED="1448985710940">
+<edge COLOR="#00ffff"/>
+<node TEXT="Cr&#xe9;er script" ID="ID_693659133" CREATED="1448985717996" MODIFIED="1448985729071">
+<node TEXT="Cr&#xe9;er lien entre les salles" ID="ID_1444433246" CREATED="1448985740060" MODIFIED="1448985745152"/>
+<node TEXT="Associer objet &#xe0; salle" ID="ID_1366079536" CREATED="1448985745779" MODIFIED="1448985759575"/>
+<node TEXT="Associer cons&#xe9;quence &#xe0; objet" ID="ID_530643130" CREATED="1448985785061" MODIFIED="1448985790466"/>
+</node>
+<node TEXT="Cr&#xe9;er &#xe9;l&#xe9;ments" ID="ID_1199726480" CREATED="1448985812188" MODIFIED="1448986427316">
+<node TEXT="" ID="ID_1615681740" CREATED="1448985816642" MODIFIED="1448985816642">
+<node TEXT="Cr&#xe9;er salle" ID="ID_707989141" CREATED="1448985816762" MODIFIED="1448985821985"/>
+<node TEXT="Cr&#xe9;er objet" ID="ID_480339347" CREATED="1448985822242" MODIFIED="1448985827585"/>
+<node TEXT="Cr&#xe9;er cons&#xe9;quence" ID="ID_1394830575" CREATED="1448985828014" MODIFIED="1448986444211"/>
+<node TEXT="Cr&#xe9;er emplacement de la carte" ID="ID_1767947489" CREATED="1448985979852" MODIFIED="1448986481477"/>
+</node>
+</node>
+<node TEXT="Sauvegarder carte" ID="ID_1865013149" CREATED="1448986006911" MODIFIED="1448986017869"/>
+<node TEXT="Charger carte" ID="ID_1592325108" CREATED="1448986029440" MODIFIED="1448986034735"/>
+</node>
+</node>
+</map>
