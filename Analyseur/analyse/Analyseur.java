a420f9966a4c165a876ac6dbf5a38c1ee806ad43
trivino2u <laura.trivino5@etu.univ-lorraine.fr>
2015-12-27 15:36:02 +0100

    Quelques méthodes pour analyseur

--- Analyseur/analyse/Analyseur.java
+++ Analyseur/analyse/Analyseur.java
@@ -3,10 +3,30 @@
  */
 package analyse;
 
+import dictionnaire.*;
+
 /**
  * @author Laura
  *
  */
 public class Analyseur {
+
+	private Dictionnaire dico;
+	private String[] aAnalyser;
+	/**
+	 * constructeur 
+	 */
+	public Analyseur(Dictionnaire dico, String[] tab){
+		this.dico = dico;
+		aAnalyser = tab;
+		
+	}
+	
+	/**
+	 * méthode qui va analyser le tableau 
+	 */
+	public static void analyserPhrase(){
+		
+	}
 
 }
