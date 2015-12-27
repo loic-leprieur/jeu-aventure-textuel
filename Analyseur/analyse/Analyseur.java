fd239243fc5cd229729ff369a69b6305e5b3adb8
trivino2u <laura.trivino5@etu.univ-lorraine.fr>
2015-12-27 16:23:44 +0100

    Petite avancée sur l'analyse

--- Analyseur/analyse/Analyseur.java
+++ Analyseur/analyse/Analyseur.java
@@ -23,10 +23,21 @@ public class Analyseur {
 	}
 	
 	/**
-	 * methode qui va analyser la phrase et faire les appel à dictionnaire qu il faut 
+	 * methode qui va analyser la phrase et faire les appels à dictionnaire qu il faut 
 	 */
 	public static void analyserPhrase(){
+		String[] tab = explode(aAnalyser," ");
+		//nb : pour ce qui suit on a besoin d'une décomposition du dico 
 		
+		if(tab.length==2){
+			//le premier est un verbe le deuxieme est un nom 
+		}
+		if(tab.length==3){
+			//le prmier est un verbe, le deuxieme est une preposition, le troisieme est un nom 
+		}
+		if(tab.length==4){
+			//le premier est un verble, le deuxieme est un nom, le troisieme une preposition, le quatrieme un nom
+		}
 	}
 
 }
