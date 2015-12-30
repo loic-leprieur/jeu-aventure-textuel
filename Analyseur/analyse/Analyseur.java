5b9a3a00be62a9e88282952ea7b1e8ecc4ba24c4
trivino2u <laura.trivino5@etu.univ-lorraine.fr>
2015-12-30 13:09:03 +0100

    Ajout des méthodes pour faciliter une éventuelle modification. Manque encore des éléments pour pouvoir complètement implémenter.

--- Analyseur/analyse/Analyseur.java
+++ Analyseur/analyse/Analyseur.java
@@ -19,34 +19,84 @@ public class Analyseur {
 	public Analyseur(Dictionnaire dico, String ph){
 		this.dico = dico;
 		aAnalyser = ph;
-		
+
 	}
-	
+
 	/**
-	 * methode qui va analyser la phrase et faire les appels à dictionnaire qu il faut 
+	 * methode qui va analyser la phrase et faire les appels ï¿½ dictionnaire qu il faut 
 	 */
 	public static void analyserPhrase(){
+		//TODO pas assez d'Ã©lÃ©ments pour finir vraiment 
 		String[] tab = aAnalyser.split(" ");
-		//nb : pour ce qui suit on a besoin d'une décomposition du dico 
-		
+		//nb : pour ce qui suit on a besoin d'une dï¿½composition du dico 
+
+		//note sur les longueurs : correspond aux regles de syntaxe
 		if(tab.length==2){
 			//le premier est un verbe le deuxieme est un nom 
 			//comparaison du premier mot avec les VERBES du dico etc 
-			//si non présent : phrase non comprise
+			//si non prï¿½sent : phrase non comprise
+			if(comparerVerbe(tab[0]) && comparerNom(tab[1])){
+
+			}else{
+				//faire en sorte dafficher message d erreur 
+			}
 		}
 		if(tab.length==3){
 			//le prmier est un verbe, le deuxieme est une preposition, le troisieme est un nom 
 			//comparaison du premier mot avec les VERBES du dico etc 
-			//si non présent : phrase non comprise
+			//si non prï¿½sent : phrase non comprise
+			if(comparerVerbe(tab[0]) && comparerPrepo(tab[1]) && comparerNom(tab[2]) ){
+
+			}else{
+				//faire en sorte dafficher message d erreur 
+			}
+
 		}
 		if(tab.length==4){
 			//le premier est un verbe, le deuxieme est un nom, le troisieme une preposition, le quatrieme un nom
 			//comparaison du premier mot avec les VERBES du dico etc 
-			//si non présent : phrase non comprise
+			//si non prï¿½sent : phrase non comprise
+			if(comparerVerbe(tab[0]) && comparerNom(tab[1]) && comparerPrepo(tab[2]) && comparerNom(tab[3]) ){
+
+			}else{
+				//faire en sorte dafficher message d erreur 
+			}
+
 		}
 		if(tab.length<=1 || tab.length>=5){
 			//leve le cas de phrase incorrecte 
 		}
 	}
 
+	//TODO : implÃ©menter, voir pour rÃ©agencer sous la forme d'une classe abstraite comparaison 
+	/**
+	 * mÃ©thode pour comparer les verbes avec le dictionnaire 
+	 */
+	public static boolean comparerVerbe(String s){
+		boolean res = false;
+		//si on trouve on met vrai 
+		return res;
+
+	}
+
+	/**
+	 * mÃ©thode pour comparer les prepositions avec le dictionnaire 
+	 */
+	public static boolean comparerPrepo(String s){
+		boolean res = false;
+		//si on trouve on met vrai 
+		return res;
+
+	}
+
+
+	/**
+	 * mÃ©thode pour comparer les nom avec le dictionnaire 
+	 */
+	public static boolean comparerNom(String s){
+		boolean res = false;
+		//si on trouve on met vrai 
+		return res;
+
+	}
 }
