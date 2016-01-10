f82fb63d38d93ff9ed7f8ee4a3faf16da1f2eba5
Loulouw <louis.zwawiak6@etu.univ-lorrraine.fr>
2016-01-10 20:32:13 +0100

    Changement dans analyser

--- Jeu d'Aventure Textuel/src/source/moteur/regles/Regle.java
+++ Jeu d'Aventure Textuel/src/source/moteur/regles/Regle.java
@@ -6,11 +6,19 @@ public class Regle {
 
     public static String analyser(Phrase phrase){
         String res = "Phrase Incorrecte";
-        if(phrase.getVerbe().equals("manger")){
-            if(phrase.getComplement().equals("pomme")){
-                res = "Miam cette pomme est délicieuse";
-            }
+        switch (phrase.getVerbe()){
+            case "manger":
+                switch (phrase.getComplement()){
+                    case "pomme":
+                        res = "Miam cette pomme est délicieuse";
+                        break;
+                    case "frite":
+                        res = "Une frite en moins";
+                        break;
+                }
+                break;
         }
+
         return res;
     }
 }
